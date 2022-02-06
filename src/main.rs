9e961e6da3760e794dc1bcd66364f98d1af8a15e
Alec Savvy <alecsavvy@outlook.com>
2022-02-06 10:12:20 -0700

    add chain reader and webserver

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,36 @@
 use axum::{routing::get, Router};
-use error::AppError;
-use std::net::SocketAddr;
-
-mod error;
-mod tokens;
+use std::{net::SocketAddr, time::Duration};
+use tokio::time::interval;
 
 #[tokio::main]
-async fn main() -> Result<(), error::AppError> {
-    let app = Router::new().route("/health", get(health));
+async fn main() {
+    // run all tasks concurrently
+    // TODO: add error handling for each future
+    let (_reader, _server) = tokio::join!(chain_reader(), web_server());
+}
+
+/**
+ * Reads chain blocks for events on an interval.
+ */
+async fn chain_reader() {
+    let mut reading_interval = interval(Duration::from_secs(3));
+    loop {
+        reading_interval.tick().await;
+        println!("reading chain block");
+    }
+}
+
+async fn web_server() {
+    let app = Router::new().route("/", get(health));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
-        .map_err(|e| AppError::IdkMan(e.to_string()))?;
-
-    Ok(())
+        .expect("could not stand up server");
 }
 
-// health check
 async fn health() -> &'static str {
-    "Up!"
+    println!("received request");
+    "up"
 }
